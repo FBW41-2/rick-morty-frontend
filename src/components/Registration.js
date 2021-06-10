edd5a14f3c4c3644da910a2beef2fc8515103056
galymax <werkhowski@gmail.com>
2021-06-10 17:47:58 +0200

    check password match

--- src/components/Registration.js
+++ src/components/Registration.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { useForm } from 'react-hook-form'
 
 export default () => {
-    const { register, formState: { errors }, handleSubmit } = useForm()
+    const { register, formState: { errors }, handleSubmit, watch } = useForm()
     const submit = (data) => {
         // send data to backend
         console.log(data)
     }
+
     return (
         <form onSubmit={handleSubmit(submit)}>
             <div>
@@ -21,6 +22,12 @@ export default () => {
                 <input {...register("password", {required: true, minLength: 8})} type="password" placeholder="Password" />
             </div>
             {errors.password && <div>Password is required</div>}
+            <div>
+                <input {...register("password_repeat", {validate: (value) => {
+                        return value === watch('password'); // value is from password_repeat and watch will return value from password
+                    }})} placeholder="repeat Password" type="password" />
+            </div>
+            {errors.password_repeat && <div>Password is not the same</div>}
             <input type="submit" />
         </form>
     )
